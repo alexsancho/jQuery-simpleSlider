3ec8e7d16847aa3c66653ee9050c4f5b7bc4b1bf
Alex Sancho <alex@alexsancho.name>
2012-03-23 23:24:09 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+jQuery simpleSlider Plugin
+--------------------------
+
+This plugin converts a group of figure elements in a simple slideshow.
+
+Usage
+-----
+
+First include this script after jQuery.
+
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="jQuery.simpleSlider.min.js"></script>
+	
+Then you only need to call the method using any selector that contains a group of figure elements.
+
+	<script>
+		jQuery(function() {
+			jQuery('.myslider').simpleSlider();
+		});
+	</script>
+
+Acknowledgements
+----------------
+
+- [Iridexel 2](http://fifty6.deviantart.com/art/Iridexel-2-190375130)
+
+© 2012, Alex Sancho. Released under the [MIT License](http://www.opensource.org/licenses/mit-license.php).
---
+++ jQuery.simpleSlider.js
@@ -0,0 +1,100 @@
+/**
+ * jQuery SimpleSlide Plugin
+ *
+ * Copyright(c) 2012 Alex Sancho <alex@alexsancho.name>
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *    http://www.opensource.org/licenses/mit-license.php
+ *    http://www.gnu.org/licenses/gpl.html
+ */
+(function($) {
+	"use strict";
+
+	$.fn.simpleSlider = function(options) {
+		return this.each(function() {
+			new $.simpleSlider(this, options);
+		});
+	};
+
+	$.simpleSlider = function(element, options) {
+		this.options = $.extend({}, $.simpleSlider.defaults, options || {});
+		this.element = $(element);
+		this.init();
+	};
+
+	$.simpleSlider.prototype = {
+		init: function() {
+			var $this = jQuery(this.element),
+				navi = jQuery('<ul class="navigator" />'),
+				figures = jQuery('figure', $this);
+
+			$this.addClass('simpleSlider slider');
+
+			figures.each(function(index, value) {
+				var idx = index+1,
+					trigger = jQuery('<li><a href="#">'+idx+'</a></li>');
+
+				if (idx === 1) {
+					trigger.addClass('selected');
+				} else {
+					jQuery(value).hide();
+				}
+
+				trigger.bind('click', function(event) {
+					event.preventDefault();
+
+					jQuery(this)
+						.siblings()
+						.removeClass('selected');
+
+					jQuery(this)
+						.addClass('selected');
+
+					jQuery(value)
+						.siblings('figure')
+						.hide();
+
+					jQuery(value).show();
+				});
+
+				navi.append(trigger);
+			});
+
+			if (jQuery('li', navi).length > 1) {
+				var prev = jQuery('<li class="control"><a href="#"><</></li>'),
+					next = jQuery('<li class="control"><a href="#">></></li>');
+
+				prev.bind('click', function(event) {
+					event.preventDefault();
+
+					jQuery(this)
+						.siblings('.selected')
+						.prev()
+						.not('.control')
+						.click();
+				});
+
+				next.bind('click', function(event) {
+					event.preventDefault();
+
+					jQuery(this)
+						.siblings('.selected')
+						.next()
+						.not('.control')
+						.click();
+				});
+
+				navi
+					.prepend(prev)
+					.append(next);
+
+				$this.append(navi);
+			}
+		}
+	};
+
+	$.simpleSlider.defaults = {
+		
+	};
+
+}(jQuery));
